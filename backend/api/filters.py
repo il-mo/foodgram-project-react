eab86fb74cfc561045bff92892c4cd9da3ebbbea
Ilya Moiseev <ilya.moiseev95@gmail.com>
2022-03-30 17:08:30 +0300

    without poetry3

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,6 +1,5 @@
 import django_filters
 from django_filters.rest_framework import filters
-from rest_framework.exceptions import ParseError
 
 from recipe.models import Ingredient, Recipe, Tag
 from users.models import User
