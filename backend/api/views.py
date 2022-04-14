b252fb4ac869c9da751cedd11460b530fb579eb7
Ilya Moiseev <ilya.moiseev95@gmail.com>
2022-04-14 17:01:13 +0300

    review #2

--- backend/api/views.py
+++ backend/api/views.py
@@ -156,7 +156,8 @@ class RecipeViewSet(viewsets.ModelViewSet):
                         {
                             f'{ingredient.ingredient.id}': {
                                 'name': ingredient.ingredient.name,
-                                'measurement_unit': ingredient.ingredient.measurement_unit,
+                                'measurement_unit':
+                                    ingredient.ingredient.measurement_unit,
                                 'amount': ingredient.amount,
                             }
                         }
